0494de713835b3138edd739a4d0367e3f19203e7
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-02-10 11:42:54 -0600

    Added another line to see what happens

--- src/project/hello/Hello.java
+++ src/project/hello/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Test line");
+		System.out.println("Another line to see what happens");
 	}
 
 }
